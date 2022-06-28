cff676a50447721802867a0fd05d149db7b0157e
Sriya Chippalthurty <sriya210@gmail.com>
2022-06-28 09:45:18 -0500

    Initial Commit

---
+++ miniProject2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  miniProject2
+//
+//  Created by Scholar on 6/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
